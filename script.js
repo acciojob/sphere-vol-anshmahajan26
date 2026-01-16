cbcb4cf955022b26e873cceb3289655be53b8384
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-16 18:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function volume_sphere() {
-    //Write your code here
+   event.preventDefault();
+
+  // get radius value
+  let radius = document.getElementById("radius").value;
+  radius = Number(radius);
+
+  let volume;
+
+  // validate input
+  if (isNaN(radius) || radius < 0) {
+    volume = NaN;
+  } else {
+    volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volume = volume.toFixed(4); // round to 4 decimals
+  }
+
+  // show result
+  document.getElementById("volume").value = volume;
   
 } 
 
